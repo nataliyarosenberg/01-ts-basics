2ee9303958f628aa049b0bb7b59b1613ef510d83
nataliyarosenberg <nshcherbak@yahoo.com>
2025-08-22 10:57:52 -0700

    tasks markup

---
+++ src/task-7.ts
@@ -0,0 +1,9 @@
+function getMessage() {
+  return new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then((result) => console.log(result));
